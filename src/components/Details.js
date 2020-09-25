3df35e16668d6320efef89978d28475bc0db4dff
Hector Martinez <hectormartinez.2978@gmail.com>
2020-09-25 03:35:50 -0700

    making menu

---
+++ src/components/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Details (){
+    return (
+        <div>
+            <h2>Details:</h2>
+            <p>This website journal is about the steps I took to completing the Inmoov Robot, and by completeing</p>
+        </div>
+    )
+} 
+
+export default Details;
