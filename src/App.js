a8763a8bbce36afcf8de8653193380068cd4557c
Hector Martinez <hectormartinez.2978@gmail.com>
2020-09-25 04:19:06 -0700

    added multiple pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+
 import React from 'react';
-import Mainpage from '../src/components/Mainpage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+// import logo from './logo.svg';
+import './assets/css/main.css';
+
+//components from that html5 place
+import Mainpage from './components/Mainpage';
+import Details from './components/Details';
+import Learned from './components/Learned';
+
 function App() {
   return (
-    <div>
-      <div>Journal for inmoov build</div>
-      <Mainpage/>
-    </div>
+    <Router>
+      <div className="App">
+            <div className="tabs">
+              <Link className="tab" to="/">Mainpage</Link>
+              <Link className="tab" to="/about">Details</Link>
+              <Link className="tab" to="/portfolio">Learned</Link>
+            </div>
+        <Switch>
+          <Route exact path="/">
+            <Mainpage/>
+          </Route>
+          <Route exact path="/details">
+            <Details/>
+          </Route>
+          <Route exact path="/learned">
+            <Learned/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Learned.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Learned (){
+    return (
+        <div>
+            <h2>Learned:</h2>
+            <p>This website journal is about the steps I took to completing the Inmoov Robot, and by completeing</p>
+        </div>
+    )
+} 
+
+export default Learned;
