913cfb45103d0d5f74e3c1a7b9c05b3f55ac1872
Hector Martinez <hectormartinez.2978@gmail.com>
2020-09-28 06:38:28 -0700

    details.js planning

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,6 +5,7 @@ function Details (){
         <div>
             <h2>Details:</h2>
             <p>This website journal is about the steps I took to completing the Inmoov Robot, and by completeing</p>
+            <div>At the moment I have broken a pcb for the right arm, so now I am using a breadboard as a substitute.</div>
         </div>
     )
 } 
